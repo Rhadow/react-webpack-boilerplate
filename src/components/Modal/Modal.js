520c2cfd7cb8bc7930e282063b99f518ab0ca120
Rhadow <howard3433@gmail.com>
2015-05-26 22:12:21 +0800

    updated index.html content

---
+++ src/components/Modal/Modal.js
@@ -0,0 +1,35 @@
+require('./Modal.scss');
+
+import React from 'react';
+
+class Modal extends React.Component {
+    render() {
+        return (
+            <div className="app-modal-wrapper">
+                <button type="button" className="btn btn-primary btn-lg" data-toggle="modal" data-target="#myModal">
+                    Launch demo modal
+                </button>
+
+                <div className="modal fade" id="myModal" tabIndex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+                    <div className="modal-dialog">
+                        <div className="modal-content">
+                            <div className="modal-header">
+                                <button type="button" className="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                                <h4 className="modal-title" id="myModalLabel">Modal title</h4>
+                            </div>
+                            <div className="modal-body">
+                                Hello, I am a Modal
+                            </div>
+                            <div className="modal-footer">
+                                <button type="button" className="btn btn-default" data-dismiss="modal">Close</button>
+                                <button type="button" className="btn btn-primary">Save changes</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Modal;
