15616e3cc3e68a6ed86f5661fb485bffb9346695
Howard Chang <howard3433@gmail.com>
2015-05-26 17:07:40 +0800

    removed fake data

--- src/app.js
+++ src/app.js
@@ -2,7 +2,6 @@ require('../node_modules/bootstrap/dist/css/bootstrap.min.css');
 require('./assets/styles/app.css');
 
 import React from 'react';
-import Profile from './components/Profile/Profile.js';
 
 class App extends React.Component {
     constructor(props) {
@@ -10,9 +9,28 @@ class App extends React.Component {
     }
     render() {
         return (
-        	<div>
-                <h1>Hello Webpack!</h1>
-                <Profile />
+        	<div className="container">
+                <div className="jumbotron">
+                    <h1>Hello Webpack!!</h1>
+                    <p>Webpack boiler plate using proxies with webpack-dev-server</p>
+                </div>
+                <div className="row">
+                    <div className="col-sm-4">
+                        <h3>Column 1</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
+                        <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
+                    </div>
+                    <div className="col-sm-4">
+                        <h3>Column 2</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
+                        <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
+                    </div>
+                    <div className="col-sm-4">
+                        <h3>Column 3</h3> 
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
+                        <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
+                    </div>
+                </div>
             </div>
         );
     }
