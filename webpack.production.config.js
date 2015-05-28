0660aa099b88206533ffe047f9cdc71e2a387b9b
Howard Chang <howard3433@gmail.com>
2015-05-28 16:11:53 +0800

    disable warning for Uglify in production mode

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -45,7 +45,12 @@ var config = {
             "window.jQuery": "jquery",
             "root.jQuery": "jquery"
         }),
-        new Webpack.optimize.CommonsChunkPlugin('vendors', 'vendors.bundle.js', Infinity)
+        new Webpack.optimize.CommonsChunkPlugin('vendors', 'vendors.bundle.js', Infinity),
+        new Webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: false
+            }
+        })
     ],
     resolve: {
         extensions: ['', '.js', '.jsx', '.css', '.scss']
